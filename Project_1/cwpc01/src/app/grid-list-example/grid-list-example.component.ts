f09233fb66e3b56c6362785d1057aff9aa4fcc02
Mitesh Sheth <miiteshssheth9@gmail.com>
2025-04-12 19:20:15 +0530

    update product section

---
+++ Project_1/cwpc01/src/app/grid-list-example/grid-list-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-list-example',
+  templateUrl: './grid-list-example.component.html',
+  styleUrls: ['./grid-list-example.component.css']
+})
+export class GridListExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
